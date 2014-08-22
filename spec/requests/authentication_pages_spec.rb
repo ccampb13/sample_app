842d9e83cf2781d89e1c06a76230425bc24abec9
Chad Campbell <ccampb13@gmail.com>
2014-08-22 13:34:54 -0500

    introudcing ajax, working on getting tests to pass

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,7 +49,19 @@ describe "Authentication" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
       end
+    end
 
       describe "when attempting to visit a protected page" do
         before do
